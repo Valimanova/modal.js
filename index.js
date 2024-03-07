1b9e2c6d6316ab5a7f344fad9ca9b932501d924a
Valimanova <vusalaalimanova1@gmail.com>
2024-03-07 12:12:50 +0400

    Your commit message here

--- index.js
+++ index.js
@@ -11,21 +11,34 @@ $(document).ready(function(){
             console.log(error);
         }
     })
+    console.log(data);
 })
 
-let m0dal= document.querySelector(".m0dal");
-let images= document.querySelectorAll(".img")
-// console.log(images);
-images.forEach(element => {
-    element.addEventListener("click",function(){
-        m0dal.style.display="block"
-        m0dal.innerHTML = element.innerHTML
-        // console.log(element.innerHTML+=m0dal.innerHTML);
-    })
-    })
-    let btn= document.querySelector(".btn");
-    btn.addEventListener("click",function(){
+let all = document.querySelectorAll(".img");
+let m0dal = document.querySelector(".m0dal");
+let cl0se= document.querySelector(".fa-rectangle-xmark")
+let last= document.querySelector(".left");
+let next= document.querySelector(".right")
+let currentIndex = 0;
+
+all.forEach(element => {
+    element.addEventListener("click", function() {
+        m0dal.style.display = "block";
+        m0dal.innerHTML = "";
+        m0dal.innerHTML+= element.innerHTML
+        currentIndex = index; 
+    });
+    cl0se.addEventListener('click',function(){
         m0dal.style.display="none"
     })
-
-console.log(m0dal);
+   
+    last.addEventListener("click", function() {
+        currentIndex = (currentIndex - 1 + all.length) % all.length; 
+        m0dal.innerHTML = all[currentIndex].innerHTML; 
+    });
+    
+    next.addEventListener("click", function() {
+        currentIndex = (currentIndex + 1) % all.length; 
+        m0dal.innerHTML = all[currentIndex].innerHTML; 
+    });
+});
